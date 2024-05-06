7afe5cc608ffbb128e9c38a5d02e0f45dea84e71
Michaela202 <114591580+Michaela202@users.noreply.github.com>
2024-05-06 16:38:23 +0200

    geting the light reading and humidity/temperature

--- sep4-IoT/src/main.c
+++ sep4-IoT/src/main.c
@@ -11,6 +11,9 @@
 #include <string.h>
 #include <dht11.h>
 #include <periodic_task.h>
+#include <light.h>
+#include <stdio.h>
+
 // Buffer to hold the received message
 char received_message_buffer[128];
 
@@ -34,6 +37,14 @@ void getTemptAndHum(){
     }
 }
 
+void getLightInfo() {
+    uint16_t light_value = light_read(); 
+    char result[50];
+    sprintf(result, "LIGHT: %d\n", light_value);
+    wifi_command_TCP_transmit((uint8_t*)result, strlen(result));
+}
+
+
 void Callback(){
     pc_comm_send_string_blocking(received_message_buffer);
     wifi_command_TCP_transmit((uint8_t*)"Recieved ", 10);
@@ -53,19 +64,20 @@ int main(){
     buttons_init();
     tone_init();
     leds_init();
-    wifi_command_join_AP("Filip's Galaxy S21 FE 5G","jgeb6522");
-    wifi_command_create_TCP_connection("192.168.175.232",6868,Callback,received_message_buffer);
+    light_init();
+    wifi_command_join_AP("002","zabijemsazalentilku");
+    wifi_command_create_TCP_connection("192.168.236.153",23,Callback,received_message_buffer);
     wifi_command_TCP_transmit((uint8_t*)"Connected ", 11);
 
-    periodic_task_init_a(getTemptAndHum,120000);
+    periodic_task_init_a(getTemptAndHum,13000);
+    periodic_task_init_b(getLightInfo, 12000); // The light return values are reversed (super dark -> 1000; light -> 0)
 
-    //void(*test)();
-    //test =& getTemptAndHum;
-    //periodic_task_init_a(test,2000);
+
+   
 
     while (1)
     {
-        /* code */
+     
     }
     
     
