da62cdb32388dafa8005ef9f8e0ca5d14046043a
KaBence <kabaly.bence@gmail.com>
2024-05-08 12:45:01 +0200

    clean up and reorganizing of the code

--- sep4-IoT/src/main.c
+++ sep4-IoT/src/main.c
@@ -9,12 +9,12 @@
 #include <util/delay.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dht11.h>
 #include <periodic_task.h>
-#include <light.h>
 #include <stdio.h>
 
-#include <stdbool.h>
+#include "Utility.h"
+#include "TempAndHum.h"
+#include "LightInfo.h"
 
 
 #include "uECC.h"
@@ -27,72 +27,14 @@ struct AES_ctx my_AES_ctx;
 // Buffer to hold the received message
 char received_message_buffer[128];
 
+void transmitData(uint8_t * data,uint16_t length){
+    AES_ECB_encrypt(&my_AES_ctx,(uint8_t*)data);
+    wifi_command_TCP_transmit((uint8_t*)data,length);
 
-bool contains(const char *haystack, const char *needle) {
-    return strstr(haystack, needle) != NULL;
-}
+    AES_ECB_decrypt(&my_AES_ctx,(uint8_t*)data);
+    pc_comm_send_array_blocking((uint8_t*)data,length);
 
-char** split(const char* str, const char* delim, int* num_tokens) {
-    // Count the number of tokens
-    int count = 1; // At least one token exists
-    const char* tmp = str;
-    while ((tmp = strstr(tmp, delim))) {
-        count++;
-        tmp += strlen(delim); // Move pointer past the delimiter
-    }
-
-    // Allocate memory for array of pointers
-    char** tokens = (char**)malloc(count * sizeof(char*));
-    if (tokens == NULL) {
-        return NULL; // Memory allocation failed
-    }
-
-    // Split the string
-    int i = 0;
-    char* token = strtok((char*)str, delim);
-    while (token != NULL) {
-        tokens[i++] = token;
-        token = strtok(NULL, delim);
-    }
-
-    *num_tokens = count;
-    return tokens;
-}
-
-
-
-void getTemptAndHum(){
-    uint8_t humidity_integer = 0; 
-    uint8_t  humidity_decimal = 0; 
-    uint8_t temperature_integer = 0; 
-    
-    uint8_t temperature_decimal = 0;
-
-    DHT11_ERROR_MESSAGE_t status = dht11_get(&humidity_integer,&humidity_decimal,&temperature_integer,&temperature_decimal);
-    char result[16];
-
-
-    if (status == DHT11_OK)
-    {
-        sprintf(result, "T:%d.%d    H:%d.%d", temperature_integer, temperature_decimal, humidity_integer, humidity_decimal); //might not be the null terminator at the end
-        //AES_CBC_encrypt_buffer(&my_AES_ctx,result,strlen(result));
-        AES_ECB_encrypt(&my_AES_ctx,(uint8_t*)result);
-        wifi_command_TCP_transmit((uint8_t*)result,15);
-
-        //AES_CBC_decrypt_buffer(&my_AES_ctx,result,strlen(result));
-        AES_ECB_decrypt(&my_AES_ctx,(uint8_t*)result);
-        pc_comm_send_array_blocking((uint8_t*)result,16);
-    }
-    else{
-        wifi_command_TCP_transmit((uint8_t*)"Temp Hum Error ",16);
-    }
-}
-
-void getLightInfo() {
-    uint16_t light_value = light_read(); 
-    char result[50];
-    sprintf(result, "LIGHT: %d\n", light_value);
-    wifi_command_TCP_transmit((uint8_t*)result, strlen(result));
+    free(data);
 }
 
 
@@ -134,23 +76,32 @@ void setup(){
 
     //wifi_command_join_AP("Filip's Galaxy S21 FE 5G","jgeb6522");
     wifi_command_join_AP("KBENCELT 3517","p31A05)1");
-
     wifi_command_create_TCP_connection("192.168.137.1",6868,Callback,received_message_buffer);
-}
 
-
-
-
-int main(){
-    setup();
     char* public_key_hex = print_hex(getIOTPublicKey(&enc), 64);
     char* connection = (char*)malloc((sizeof("Connected:") + strlen(public_key_hex) + 1) * sizeof(char));
     sprintf(connection, "Connected:%s", public_key_hex);
     wifi_command_TCP_transmit((uint8_t*)connection,strlen(connection));
     free(connection);
+}
 
-    periodic_task_init_a(getTemptAndHum,13000);
-    periodic_task_init_b(getLightInfo,12000);
+void sendTempAndHumidity(){
+    uint8_t *data = getTempAndHum(); 
+    transmitData(data, 16);
+
+}
+
+void sendLight(){
+    uint8_t *data = getLightInfo(); 
+    transmitData(data,16);
+}
+
+
+int main(){
+    setup();
+    
+    periodic_task_init_a(sendTempAndHumidity,13000);
+    periodic_task_init_b(sendLight,12000);
 
 
     while (1)
