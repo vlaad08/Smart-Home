835a501ab99a86208f3b3d67f8c72e7afc9f1713
Vlad-Alexandru Nita <vlad.alexandru194@gmail.com>
2024-05-10 09:04:39 +0200

    fixed rmed method

--- Backend-SEP4/TCPListener/ICommunicator.cs
+++ Backend-SEP4/TCPListener/ICommunicator.cs
@@ -2,8 +2,6 @@
 
 public interface ICommunicator
 { 
-
-    public string getTemperature();
     public void setTemperature(string hardwareId, int level);
     public void setLight(string hardwareId, int level);
 
