1dde1f0cfc1c95065dea6c4a58757c945a7b4360
Ágoston Babicz <babiczagoston3@gmail.com>
2024-05-13 16:20:49 +0200

    Small conflict fix

--- Backend-SEP4/Intermediary-DB/Logic/Interfaces/IAccountLogic.cs
+++ Backend-SEP4/Intermediary-DB/Logic/Interfaces/IAccountLogic.cs
@@ -3,9 +3,6 @@
 public interface IAccountLogic
 {
 
-    Task<Member> GetAdmin(string login, string password);
-    Task RegisterMember(string username, string password);
-    Task<Member> GetMember(String login, string password);
     Task Delete(string username, string password);
     Task EditUsername(string oldUsername, string newUsername,string password);
     Task EditPassword(string username, string oldPassword, string newPassword);
