37dfde8423d53f632c2c0c437f8392fc648fd8c6
DominikaJanczyszyn <119040751+DominikaJanczyszyn@users.noreply.github.com>
2024-05-21 14:52:28 +0200

    commit

--- Backend-SEP4/WebAPI/Controllers/RoomController.cs
+++ Backend-SEP4/WebAPI/Controllers/RoomController.cs
@@ -19,6 +19,7 @@ public class RoomController : ControllerBase
     {
         this.logic = logic;
     }
+    
     //An endpoint to get all rooms based on a specific houseId (request with houseId, return a list of rooms with their ids, names, current temperature, humidity, light level)
     [HttpGet("{houseId}")]
     public async Task<ActionResult<List<RoomDataTransferDTO>>> GetAllRooms([FromRoute] string houseId)
