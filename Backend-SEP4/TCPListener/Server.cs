7dbb8e2804edfbd2187e41c9618cd1df84dcb565
vlaad08 <vlad.alexandru194@gmail.com>
2024-05-28 09:47:50 +0200

    added deploy files

--- Backend-SEP4/TCPListener/Server.cs
+++ Backend-SEP4/TCPListener/Server.cs
@@ -9,9 +9,10 @@ public class Server
     private TcpListener listener;
     private Thread serverThread;
     private bool isRunning;
-    private IEncryptionService enc = new EncryptionService("S3cor3P45Sw0rD@f"u8.ToArray(),null);
+    private IEncryptionService enc = new EncryptionService("S3cor3P45Sw0rD@f"u8.ToArray(), null);
     private static readonly HttpClient httpClient = new HttpClient();
     private NetworkStream stream;
+
 
 
     /* A dictionary to store all connected clients and a boolean value to check if they are logic clients or not.
@@ -19,10 +20,22 @@ public class Server
     */
     private IDictionary<TcpClient, bool> allClients = new Dictionary<TcpClient, bool>();
 
-    
+    private string SERVER_ADDRESS;
+
     public Server(int port)
     {
-        string SERVER_ADDRESS = Environment.GetEnvironmentVariable("SERVER_ADDRESS") ?? "127.0.0.1";
+        DotNetEnv.Env.TraversePath().Load();
+
+        SERVER_ADDRESS = Environment.GetEnvironmentVariable("SERVER_ADDRESS");
+
+        if (SERVER_ADDRESS == null)
+        {
+            DotNetEnv.Env.Load();
+        }
+
+        SERVER_ADDRESS = Environment.GetEnvironmentVariable("SERVER_ADDRESS") ?? "127.0.0.1";
+
+
         IPAddress localAddr = IPAddress.Parse(SERVER_ADDRESS);
         Console.WriteLine("Server address: " + localAddr.ToString());
         listener = new TcpListener(localAddr, port);
@@ -30,7 +43,7 @@ public class Server
         listener.Start();
         ListenForClients();
     }
-    
+
     private void ListenForClients()
     {
         Console.WriteLine("Server started, listening for clients...");
@@ -64,7 +77,7 @@ public class Server
         while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
         {
             receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-            
+
 
 
             string decryptedData = enc.Decrypt(buffer.Take(bytesRead).ToArray());
@@ -79,92 +92,92 @@ public class Server
             switch (decryptedData)
             {
                 case var message when message.StartsWith("LOGIC:"):
-                {
-                    
-                    string[] parts = message.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    string newMessage = string.Join(" ", parts.Skip(1));
-                    foreach (var logicClient in allClients)
                     {
-                        if (!logicClient.Value)
+
+                        string[] parts = message.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        string newMessage = string.Join(" ", parts.Skip(1));
+                        foreach (var logicClient in allClients)
                         {
-                            int blockSize = 16; 
-                            int extraBytes = newMessage.Length % blockSize;
-                            if (extraBytes != 0)
+                            if (!logicClient.Value)
                             {
-                                newMessage = newMessage.PadRight(newMessage.Length + blockSize - extraBytes, ' ');
+                                int blockSize = 16;
+                                int extraBytes = newMessage.Length % blockSize;
+                                if (extraBytes != 0)
+                                {
+                                    newMessage = newMessage.PadRight(newMessage.Length + blockSize - extraBytes, ' ');
+                                }
+                                byte[] messageBytes = enc.Encrypt(newMessage);
+                                Send(messageBytes, logicClient.Key);
                             }
-                            byte[] messageBytes = enc.Encrypt(newMessage);
-                            Send(messageBytes, logicClient.Key);
                         }
                     }
-                }
-                break;
+                    break;
 
                 case var message when message.Contains("T:") && message.Contains("H:"):
-                {
-                    string[] parts = message.Split(new[] { ' ', '-', ':', 'H' }, StringSplitOptions.RemoveEmptyEntries);
-
-                    if (parts.Length >= 4)
                     {
-                        string deviceId = parts[0];
-                        string tempString = parts[2];
-                        string humString = parts[3];
+                        string[] parts = message.Split(new[] { ' ', '-', ':', 'H' }, StringSplitOptions.RemoveEmptyEntries);
 
-                        if (double.TryParse(tempString, out double tempValue) && double.TryParse(humString, out double humValue))
+                        if (parts.Length >= 4)
                         {
-                            await SaveTemperatureAsync(deviceId, tempValue);
-                            await SaveHumidityAsync(deviceId, humValue);
+                            string deviceId = parts[0];
+                            string tempString = parts[2];
+                            string humString = parts[3];
+
+                            if (double.TryParse(tempString, out double tempValue) && double.TryParse(humString, out double humValue))
+                            {
+                                await SaveTemperatureAsync(deviceId, tempValue);
+                                await SaveHumidityAsync(deviceId, humValue);
+                            }
                         }
                     }
-                }
-                break;
+                    break;
 
                 case var message when message.Contains("LIGHT:"):
-                {
-                    string[] parts = message.Substring(2).Split(new[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
-
-                    if (parts.Length > 1 && double.TryParse(parts[1], out double lightValue))
                     {
-                        string deviceId = message.Substring(0, 1);
-                        await SaveLightAsync(deviceId, lightValue);
+                        string[] parts = message.Substring(2).Split(new[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (parts.Length > 1 && double.TryParse(parts[1], out double lightValue))
+                        {
+                            string deviceId = message.Substring(0, 1);
+                            await SaveLightAsync(deviceId, lightValue);
+                        }
                     }
-                }
-                break;
+                    break;
 
                 default:
                     break;
-                {
-                    Console.WriteLine("switchcase default");
-                    // The first 4 characters will be used by the receiving party
-                    string identifier = decryptedData.Substring(0, 4);
-                    Console.WriteLine("switchcase default 1");
-
-                    string remainingMessage = decryptedData.Substring(4);
-                    Console.WriteLine("switchcase default 2");
-
-                    foreach (var logicClient in allClients)
                     {
-                        Console.WriteLine("switchcase default 3");
+                        Console.WriteLine("switchcase default");
+                        // The first 4 characters will be used by the receiving party
+                        string identifier = decryptedData.Substring(0, 4);
+                        Console.WriteLine("switchcase default 1");
 
-                        if (!logicClient.Value)
-                        {                    
-                            Console.WriteLine("switchcase default4");
+                        string remainingMessage = decryptedData.Substring(4);
+                        Console.WriteLine("switchcase default 2");
 
-                            int blockSize = 16;
-                            int extraBytes = remainingMessage.Length % blockSize;
-                            if (extraBytes != 0)
+                        foreach (var logicClient in allClients)
+                        {
+                            Console.WriteLine("switchcase default 3");
+
+                            if (!logicClient.Value)
                             {
-                                remainingMessage = remainingMessage.PadRight(remainingMessage.Length + blockSize - extraBytes, ' ');
+                                Console.WriteLine("switchcase default4");
+
+                                int blockSize = 16;
+                                int extraBytes = remainingMessage.Length % blockSize;
+                                if (extraBytes != 0)
+                                {
+                                    remainingMessage = remainingMessage.PadRight(remainingMessage.Length + blockSize - extraBytes, ' ');
+                                }
+                                byte[] messageBytes = enc.Encrypt(identifier + remainingMessage);
+                                Console.WriteLine("switchcase default 5");
+
+                                Send(messageBytes, logicClient.Key);
+                                Console.WriteLine("switchcase default 6");
+
                             }
-                            byte[] messageBytes = enc.Encrypt(identifier + remainingMessage);
-                            Console.WriteLine("switchcase default 5");
-
-                            Send(messageBytes, logicClient.Key);
-                            Console.WriteLine("switchcase default 6");
-
                         }
                     }
-                } 
                     break;
                     Console.WriteLine("Unrecognized message format.");
                     //break;
@@ -195,7 +208,7 @@ public class Server
     {
         try
         {
-            HttpResponseMessage response = await httpClient.PostAsync($"http://localhost:5084/temperature/devices/{deviceId}/{value}", null);
+            HttpResponseMessage response = await httpClient.PostAsync($"http://{SERVER_ADDRESS}:80/temperature/devices/{deviceId}/{value}", null);
 
             // Console.WriteLine(response.ToString());
             if (response.IsSuccessStatusCode)
@@ -217,7 +230,7 @@ public class Server
     {
         try
         {
-            HttpResponseMessage response = await httpClient.PostAsync($"http://localhost:5084/humidity/devices/{deviceId}/{value}", null);
+            HttpResponseMessage response = await httpClient.PostAsync($"http://{SERVER_ADDRESS}:80/humidity/devices/{deviceId}/{value}", null);
 
             // Console.WriteLine(response.ToString());
             if (response.IsSuccessStatusCode)
@@ -239,7 +252,7 @@ public class Server
     {
         try
         {
-            HttpResponseMessage response = await httpClient.PostAsync($"http://localhost:5084/light/devices/{deviceId}/{value}", null);
+            HttpResponseMessage response = await httpClient.PostAsync($"http://{SERVER_ADDRESS}:80/light/devices/{deviceId}/{value}", null);
 
             // Console.WriteLine(response.ToString());
             if (response.IsSuccessStatusCode)
